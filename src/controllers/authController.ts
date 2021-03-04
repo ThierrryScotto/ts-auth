073e37ae5823729f435a3f56f9f26c3586af6634
thierrryScotto <thierry_192@outlook.com>
2021-03-03 23:02:35 -0300

    added autentication

---
+++ src/controllers/authController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import User from '../app/models/User';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+class UserController{
+  public async authenticate(req: Request, res: Response) {
+    const repository = getRepository(User);
+  
+    const { email, password } = req.body;
+
+    const user = await repository.findOne({ where: { email }});
+    if (!user) {
+      return res.sendStatus(401);
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      return res.sendStatus(401);
+    }
+
+    const token = jwt.sign({ id: user.id }, 'secret', { expiresIn: '1d' });
+
+    return res.status(201).json({ user, token });
+  }
+}
+
+export default new UserController();
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import authController from './controllers/authController';
 import UserController from './controllers/userController';
 
 const router = Router();
 
 router.post('/users', UserController.store);
+router.post('/auth', authController.authenticate);
 
 export default router;
