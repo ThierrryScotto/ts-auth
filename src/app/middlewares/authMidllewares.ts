c48c1b322d8f1f2e3ae460d8c5aba64f92a1647b
thierrryScotto <thierry_192@outlook.com>
2021-03-03 23:26:56 -0300

    fix jsonwebtoken

--- src/app/middlewares/authMidllewares.ts
+++ src/app/middlewares/authMidllewares.ts
@@ -1,6 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
+interface TokenPayload {
+  id: string;
+  iat: number;
+  exp: number;
+}
+
 export default function authMiddleware(
   req: Request,
   res: Response,
@@ -15,6 +21,10 @@ export default function authMiddleware(
   next();
   try {
     const data = jwt.verify(token, 'secret');
+
+    const { id } = data as TokenPayload;
+
+    req.userId = id;
   } catch {
     return res.sendStatus(401);
   }
